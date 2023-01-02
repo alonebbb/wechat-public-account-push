de5299425239843a735e7b0e9cf7a5be6efa4681
alonebbb <121752593+alonebbb@users.noreply.github.com>
2023-01-02 19:59:04 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -6,7 +6,7 @@
  * 自行添加属性，以支持更多个性化功能
  */
 const USER_CONFIG = {
-USE_PASSAGE: 'push-plus',
+USE_PASSAGE: 'server-chan',
   // 使用微信测试号：公众号APP_ID
   APP_ID: 'wxc1c4e523e75e7f16',
 
@@ -21,7 +21,7 @@ USE_PASSAGE: 'push-plus',
       // 想要发送的人的名字
       name: '李佳音',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '2fbecf206d0a4aea837d9fad1d6bbc8e',
+      id: 'SCT190382T7802myL09kqF42othranQlFD',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '0001',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
@@ -51,13 +51,13 @@ USE_PASSAGE: 'push-plus',
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'wNHsHpIo-dNEq3s7V7gz-F3ipoRPUMvMpzofsIr--o8',
+  CALLBACK_TEMPLATE_ID: '0001',
 
   CALLBACK_USERS: [
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'o_ZSp6EIVs2LBBhmnBleCnxOHZAM',
+      id: 'SCT190381TabbH9E4flsZ5sJGnbEFMcpPF',
     }
   ],
 
