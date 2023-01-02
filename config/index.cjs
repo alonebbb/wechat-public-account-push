ec5f7bda7ef288d12bb13906011c4f5ad217eb62
alonebbb <121752593+alonebbb@users.noreply.github.com>
2023-01-02 23:04:54 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -6,7 +6,7 @@
  * 自行添加属性，以支持更多个性化功能
  */
 const USER_CONFIG = {
-USE_PASSAGE: 'server-chan',
+
   // 使用微信测试号：公众号APP_ID
   APP_ID: 'wxc1c4e523e75e7f16',
 
@@ -21,15 +21,15 @@ USE_PASSAGE: 'server-chan',
       // 想要发送的人的名字
       name: '李佳音',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'SCT190382T7802myL09kqF42othranQlFD',
+      id: 'wNHsHpIo-dNEq3s7V7gz-F3ipoRPUMvMpzofsIr--o8',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '0001',
+      useTemplateId: 'o_ZSp6MOWbJowrILMpy40KvFmd8E',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '08-31',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '李佳音', year: '2004', date: '07-16',
+         // type: '*生日', name: '李佳音', year: '2004', date: '07-16',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
@@ -51,13 +51,13 @@ USE_PASSAGE: 'server-chan',
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '0001',
+  CALLBACK_TEMPLATE_ID: 'wNHsHpIo-dNEq3s7V7gz-F3ipoRPUMvMpzofsIr--o8',
 
   CALLBACK_USERS: [
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'SCT190381TabbH9E4flsZ5sJGnbEFMcpPF',
+      id: 'o_ZSp6EIVs2LBBhmnBleCnxOHZAM',
     }
   ],
 
