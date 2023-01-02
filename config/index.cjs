e99cb30e24a88de7fbcb4a9c83a1542f2610e2be
alonebbb <121752593+alonebbb@users.noreply.github.com>
2023-01-02 19:35:30 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -6,7 +6,7 @@
  * 自行添加属性，以支持更多个性化功能
  */
 const USER_CONFIG = {
-
+USE_PASSAGE: 'push-plus',
   // 使用微信测试号：公众号APP_ID
   APP_ID: 'wxc1c4e523e75e7f16',
 
@@ -21,9 +21,9 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '李佳音',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'o_ZSp6MOWbJowrILMpy40KvFmd8E',
+      id: '2fbecf206d0a4aea837d9fad1d6bbc8e',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'wNHsHpIo-dNEq3s7V7gz-F3ipoRPUMvMpzofsIr--o8',
+      useTemplateId: '0001',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '08-31',
       festivals: [
